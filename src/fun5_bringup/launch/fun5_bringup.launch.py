b546ca425dbe143b850e5c96fe788200c02c4768
ThepokKung <pok23250@gmail.com>
2024-09-27 18:45:41 +0700

    Fix bug

--- src/fun5_bringup/launch/fun5_bringup.launch.py
+++ src/fun5_bringup/launch/fun5_bringup.launch.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python3
-
-"""
-This program is free software: you can redistribute it and/or modify it 
-under the terms of the GNU General Public License as published by the Free Software Foundation, 
-either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program. 
-If not, see <https://www.gnu.org/licenses/>.
-
-created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
-"""
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -23,7 +6,7 @@ import xacro
     
 def generate_launch_description():
     
-    pkg = get_package_share_directory('example_description')
+    pkg = get_package_share_directory('fun5_bringup')
     rviz_path = os.path.join(pkg,'config','display.rviz')
     rviz = Node(
         package='rviz2',
