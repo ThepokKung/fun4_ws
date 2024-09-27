4f4e616d3913a9970b49879636a6c2b28e30214f
ThepokKung <pok23250@gmail.com>
2024-09-27 17:09:28 +0700

    Init commit

---
+++ src/fun5_bringup/scripts/randomtarget_node.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import yaml.loader
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import PoseStamped
+from fun5_interfaces.srv import Gettarget
+from ament_index_python.packages import get_package_share_directory
+import os, yaml, random
+import numpy as np
+
+class RandomtargetposeNode(Node):
+    def __init__(self):
+        super().__init__('randomtargetpose_node')
+        """PUB"""
+        self.target_pub = self.create_publisher(PoseStamped,'/target',10) # Target Pub
+
+        """CSV PATH"""
+        csv_file_path = os.path.join(
+            get_package_share_directory('fun5_bringup'),
+            'config',
+            'workspace_points.csv'
+        )
+        self.workspace_points = np.loadtxt(csv_file_path, delimiter=',')
+
+        """SERVICE"""
+        self.create_service(Gettarget,'/get_target',self.Gettarget_callback)
+
+        """Start node text"""
+        self.get_logger().info(f'Starting {self.get_namespace()}/{self.get_name()}') 
+
+    def Gettarget_callback(self, request, response):
+        if request.getdata == True:
+            random_row = random.choice(self.workspace_points)
+            x_random, y_random, z_random = random_row
+            response.xtarget = float(x_random)
+            response.ytarget = float(y_random)
+            response.ztarget = float(z_random)
+            response.success = True
+            response.message = f'get target : x : {x_random}, y : {y_random}, z : {z_random}'
+            self.targetpub_func(x_random, y_random, z_random)
+        else:
+            response.success = False
+            response.message = "Notthing ....."
+        return response
+
+    def targetpub_func(self,x,y,z):
+        msg = PoseStamped()
+        msg.header.frame_id = 'link_0'
+        msg.header.stamp = self.get_clock().now().to_msg()
+
+        msg.pose.position.x = x /1000
+        msg.pose.position.y = y /1000
+        msg.pose.position.z = z /1000
+
+        self.get_logger().info(f'Pub target data x :{msg.pose.position.x}, y : {msg.pose.position.y},z : {msg.pose.position.z}')
+        self.target_pub.publish(msg)
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = RandomtargetposeNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__=='__main__':
+    main()
---
+++ src/fun5_bringup/src/cpp_node.cpp
@@ -0,0 +1,8 @@
+#include "fun5_bringup/cpp_header.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
